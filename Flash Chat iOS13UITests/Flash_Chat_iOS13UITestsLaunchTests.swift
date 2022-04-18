b66f82855e112f54bdce64e7a9c903534cc520d7
m00racle <yanuar.prakosa@gmail.com>
2022-04-18 08:29:34 +0700

    make segues to tableview and add UItest target

---
+++ Flash Chat iOS13UITests/Flash_Chat_iOS13UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Flash_Chat_iOS13UITestsLaunchTests.swift
+//  Flash Chat iOS13UITests
+//
+//  Created by Yanuar Heru on 18/04/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import XCTest
+
+class Flash_Chat_iOS13UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
